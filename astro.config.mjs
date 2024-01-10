3ba5428dcf1219aa55a775d0133280cf12a64914
Filip Slezak <filipslezaklab@gmail.com>
2024-01-10 12:31:34 +0100

    site basis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
 import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [mdx(), compress()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/mixins" as *;`,
+        },
+      },
+    },
+  },
 });
