a5563a81254e2f39d14933e86478e4c533aef692
Filip Slezak <filipslezaklab@gmail.com>
2024-01-25 22:15:52 +0100

    cookies accept bar

--- src/components/product/ProductFeature/ProductFeature.tsx
+++ src/components/product/ProductFeature/ProductFeature.tsx
@@ -1,55 +1,56 @@
-import { useSignal, useSignalEffect } from "@preact/signals";
-import { clsx } from 'clsx';
+import { clsx } from "clsx";
 
 import type { ComponentChildren } from "preact";
-import { useContext, useEffect, useMemo, useState } from "preact/hooks";
-import './style.scss';
-import { FeatureContext } from "./ProductFeatureContext";
+import "./style.scss";
+import { useFeaturesStore } from "./store";
 
 interface Props {
-    title: string;
-    children: ComponentChildren;
-    id: number;
+  title: string;
+  children: ComponentChildren;
+  context: string;
+  id: number;
 }
 
-export const ProductFeature = ({ title, children, id }: Props) => {
-    const {openFeature, setOpen} = useContext(FeatureContext);
+export const ProductFeature = ({ title, children, id, context }: Props) => {
+  const isActive = useFeaturesStore((s) => s.activeFeatures[context] === id);
+  const setOpen = useFeaturesStore((s) => s.setOpen);
 
-    const isOpen = openFeature === id;
 
-    return (
-        <div class="product-feature">
-            <header onClick={() => {
-                if(isOpen) {
-                    setOpen(undefined);
-                } else {
-                    setOpen(id);
-                }
-            }}>
-                <h2>{title}</h2>
-                <div class="icon-container">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                        class={clsx({
-                            expanded: isOpen
-                        })}
-                    >
-                        <path d="M8 2V14" style="stroke: var(--text-body-primary);"></path>
-                        <path d="M2 8H14" style="stroke: var(--text-body-primary);"></path>
-                    </svg>
-                </div>
-            </header>
-            <div class={clsx('content-container', {
-                expanded: isOpen
-            })}>
-                <div slot="content">
-                    {children}
-                </div>
-            </div>
+  return (
+    <div class="product-feature">
+      <header
+        onClick={() => {
+          if(isActive) {
+            setOpen(undefined, context);
+          } else {
+            setOpen(id, context);
+          }
+        }}
+      >
+        <h2>{title}</h2>
+        <div class="icon-container">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            class={clsx({
+              expanded: isActive,
+            })}
+          >
+            <path d="M8 2V14" style="stroke: var(--text-body-primary);"></path>
+            <path d="M2 8H14" style="stroke: var(--text-body-primary);"></path>
+          </svg>
         </div>
-    );
-}
+      </header>
+      <div
+        class={clsx("content-container", {
+          expanded: isActive,
+        })}
+      >
+        <div slot="content">{children}</div>
+      </div>
+    </div>
+  );
+};
