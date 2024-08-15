4ce59b82155ef1d614d2cb261ebf6e80375004f3
cpprian <cpprian456@gmail.com>
2024-08-15 12:39:08 +0200

    impl download-confirm based on newsletter-confirm page

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,15 +1,26 @@
-import clsx from 'clsx';
-import type { ComponentChildren } from 'preact';
-import './style.scss';
+import clsx from "clsx";
+import type { ComponentChildren } from "preact";
+import "./style.scss";
 
 type Props = {
-    text: string;
-    children?: ComponentChildren;
-    onClick?: () => void;
-    size?: 'small' | 'normal' | 'large';
-    type?: string;
+  text: string;
+  children?: ComponentChildren;
+  onClick?: () => void;
+  size?: "small" | "normal" | "large";
+  type?: string;
 };
 
-export const Button = ({ children, onClick, text, type = "button", size = "normal" }: Props) => {
-    return (<button type={type} class={clsx("btn", `size-${size}`)} onClick={onClick}><span>{text}</span>{children}</button>);
-}
+export const Button = ({
+  children,
+  onClick,
+  text,
+  type = "button",
+  size = "normal",
+}: Props) => {
+  return (
+    <button type={type} class={clsx("btn", `size-${size}`)} onClick={onClick}>
+      <span>{text}</span>
+      {children}
+    </button>
+  );
+};
--- src/content/pricing/premium.md
+++ src/content/pricing/premium.md
@@ -4,8 +4,9 @@ order: 2
 price: €90/month
 link: https://buy.stripe.com/28og1750E7EngdG5kl
 ---
+
 All features listed in Open Source package with:
 
-* Support via online support ticket system
-* 1 hour of direct support via Video Calls
-* Influence & prioritize features</stron>
+- Support via online support ticket system
+- 1 hour of direct support via Video Calls
+- Influence & prioritize features</stron>
