1e956e6843d77cc0ca6c71e19a87f0ab4324968e
cpprian <cpprian456@gmail.com>
2024-08-26 11:05:57 +0200

    reset style for testmonials

---
+++ src/components/Testmonials/Carousel/Carousel.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'preact';
+import styles from './style.scss';
+import { getCollection } from 'astro:content';
+
+const Carousel = () => {
+    const [testmonials, setTestmonials] = useState();
+
+    useEffect(() => {
+
+    });
+
+};
+
+export default Carousel;
