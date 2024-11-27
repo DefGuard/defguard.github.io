48d1258aac749f46adb4a2a8a6a3d17eacb6978c
Filip Ślęzak <fslezak@teonite.com>
2024-11-27 13:14:40 +0100

    roadmap

--- src/components/clientStores/appStore.tsx
+++ src/components/clientStores/appStore.tsx
@@ -7,7 +7,9 @@ const defaults: StoreValues = {
 export const useAppStore = createWithEqualityFn<Store>(
   (set) => ({
     ...defaults,
-    setState: (vals) => { set(vals); },
+    setState: (vals) => {
+      set(vals);
+    },
   }),
   Object.is,
 );
