7968600dd4eabbe26ba964142d3ac36d0b85af66
Maciej Wójcik <maciej@defguard.net>
2025-09-11 09:29:37 +0200

    add nix setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Astro development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          pnpm
+          # TS/JS LSP
+          vtsls
+        ];
+      };
+    });
+}
